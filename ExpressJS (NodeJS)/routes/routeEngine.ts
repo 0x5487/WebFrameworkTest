8305820200a05c809c4d014f8c7f418305caa2b2
Jason Lee <jawc@hotmail.com>
2014-03-24 16:28:29 +0800

    init repo

---
+++ ExpressJS (NodeJS)/routes/routeEngine.ts
@@ -0,0 +1,24 @@
+﻿///<reference path='lib.d.ts'/>
+var express = require('express');
+var router = express.Router();
+
+
+
+function start(app) {
+
+
+    router.use('/bar', function (req, res, next) {
+        res.send('hello bar');
+    });
+
+    app.use('/jason', router);
+
+
+    app.get('/', function (req, res) {
+        res.send('hello jason');
+    });
+
+};
+
+
+module.exports = start;
