a31f387317736f45f3f25711f683814c03c29433
Jason Lee <jawc@hotmail.com>
2014-05-06 15:57:40 +0800

    update expressJS

---
+++ ExpressJS/app.js
@@ -0,0 +1,52 @@
+
+/**
+ * Module dependencies.
+ */
+
+
+var express = require('express');
+var dust = require('dustjs-linkedin');
+var cons = require('consolidate');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.engine("html", cons.dust);
+app.set('views', __dirname + '/views');
+app.set('template_engine', "dust");
+app.set('view engine', 'html');
+app.set('view options', {layout: false});
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded());
+//app.use(express.methodOverride());
+//app.use(app.router);
+//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+//if ('development' == app.get('env')) {
+//  app.use(express.errorHandler());
+//}
+
+//app.get('/users', user.list);
+//app.get('/pages', page.index);
+//routeEngine(app);
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+
+app.get('/dust', function (req, res) {
+    var model = { Title:"Hello World"};
+    res.render('helloworld3.html', { Title:"Hello World"});
+});
+
+app.get('/env', function (req, res) {
+    res.send(app.get('env'));
+});
+
+app.listen(3000);
